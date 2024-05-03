4dc63627824a0ccb63401fc3408d0a952f3fb4ea
Nicolas080922 <140418025+Nicolas080922@users.noreply.github.com>
2024-05-03 15:14:31 -0500

    Add files via upload

---
+++ Python/variables/variables.py
@@ -0,0 +1,24 @@
+#Definiendo una variable con camelCase
+nombreCompleto = "Mario"
+
+#Definiendo una variable con snake_case
+nombre_completo = "Mario"
+
+#Definir una variable 
+nombre = "Mario"
+
+# concatenar con F-STRING 
+bienvenida = f"Hola  {nombreCompleto}  ¿Como estas?" 
+
+#Concatenar con +
+bienvenida = "Hola " +nombre+ "¿Como estas?"
+
+#operadores de pertinencia (in / not in)
+
+print("Mario" in bienvenida)#true
+
+print("Mario" not in bienvenida)#false
+
+
+
+ 
